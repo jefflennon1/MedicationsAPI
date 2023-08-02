86d8d8a5965ac2073a5ba4a997f765173451b1fb
jefflenno1 <jeffersonlennon1@outlook.com>
2023-08-02 18:19:31 -0300

    Initial commit: create project, create migration initial and testing migrations

---
+++ src/main/java/com/example/medications/management/MedicationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.medications.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicationsApplication.class, args);
+	}
+
+}
