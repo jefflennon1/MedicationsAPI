34f910f1c0c1c06bb1ac3829e25430dffe2ba08d
jefflenno1 <jeffersonlennon1@outlook.com>
2023-08-03 09:32:34 -0300

    Fix the endpoin to map corretelly fields of medicaments

--- src/main/java/com/example/medications/management/models/Medicament.java
+++ src/main/java/com/example/medications/management/models/Medicament.java
@@ -1,45 +1,48 @@
 package com.example.medications.management.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+ 
 @Entity(name = "MEDICATION")
 public class Medicament {
 	
-	
-
-	public Medicament() {
-		super();
-	}
-
 	@Id
 	@Column(name = "REGISTRO")
+	@JsonProperty
 	private Long REGISTRO;
 	
 	@Column(name = "PRODUTO")
+	@JsonProperty
 	private String PRODUTO;
 	
 	@Column(name = "PRINCIPIO_ATIVO")
+	@JsonProperty
 	private String PRINCIPIO_ATIVO;
 
 	@Column(name = "LABORATORIO")
+	@JsonProperty
 	private String LABORATORIO;
 	
 	@Column(name = "APRESENTACAO")
+	@JsonProperty
 	private String APRESENTACAO;
 	
 	@Column(name = "CLASSE_TERAPEUTICA")
+	@JsonProperty
 	private String CLASSE_TERAPEUTICA;
 	
 	@Column(name = "COMERCIALIZACAO_2022")
+	@JsonProperty(value = "COMERCIALIZACAO_2022")
 	private String COMERCIALIZACAO;
 	
 	@Column(name = "TARJA")
+	@JsonProperty 
 	private String TARJA;
 
-	
-	
 	
 	public Long getREGISTRO() {
 		return REGISTRO;
--- src/main/java/com/example/medications/management/resources/MedicamentsResources.java
+++ src/main/java/com/example/medications/management/resources/MedicamentsResources.java
@@ -2,10 +2,7 @@ package com.example.medications.management.resources;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,21 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.medications.management.models.Medicament;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/medicaments")
 public class MedicamentsResources {
-
-	@GetMapping("/hello")
-	public String Hello() {
-		return "Hello worda!";
-	}
-	
-	@CrossOrigin(origins = "*")
-	@PostMapping(value = "/import", 
-	consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public void importData(@RequestBody List<Medicament> medicaments) {
-		Response response = new Response();
+ 
+	@PostMapping(value = "/import")
+	public void importData(@RequestBody  List<Medicament> medicaments) {
+		//Response response = new Response();
 		 System.out.println(medicaments);
-		 
-	}
+	} 
 }
